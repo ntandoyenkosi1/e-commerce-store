f9a132f1b608b441dda71db1b1ac0386c9d6a202
ntandoyenkosi1 <s.ntando99@gmail.com>
2022-07-26 04:34:38 +0200

    Test: Added Tests for signing up and log in

---
+++ test/user.js
@@ -0,0 +1,48 @@
+process.env.NODE_ENV = "test";
+require("dotenv").config();
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let server = require("../index");
+let should = chai.should();
+chai.use(chaiHttp);
+before((done) => {
+  chai.request(server)
+    .post("/api/login")
+    .send({
+      email: process.env.EMAIL,
+      password: process.env.PASSWORD,
+    })
+    .end((err, res) => {
+      process.env.TOKEN = res.body.token;
+      done();
+    });
+});
+describe("SignUpUser", () => {
+  it("should sign up a user", (done)=> {
+    chai.request(server)
+      .post("/api/signup")
+      .send({
+        email: "testuser@gmail.com",
+        password: "strongpassword123",
+        name:"test user"
+      })
+      .end((req, res) => {
+        res.should.have.status(200)
+        done()
+      })
+  })
+})
+describe("LoginUser", () => {
+  it("should login a user", (done) => {
+    chai.request(server)
+      .post("/api/login")
+      .send({
+        email: "testuser@gmail.com",
+        password:"strongpassword123"
+      })
+      .end((err, res) => {
+        res.should.have.status(200)
+        done();
+      });
+  })
+})
