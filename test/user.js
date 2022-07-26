0fa78dedef02206e15e7ec16080deb9135d59b71
ntandoyenkosi1 <s.ntando99@gmail.com>
2022-07-26 04:57:50 +0200

    Test: Added a test case for deleting a user

--- test/user.js
+++ test/user.js
@@ -6,43 +6,55 @@ let server = require("../index");
 let should = chai.should();
 chai.use(chaiHttp);
 before((done) => {
-  chai.request(server)
-    .post("/api/login")
-    .send({
-      email: process.env.EMAIL,
-      password: process.env.PASSWORD,
-    })
-    .end((err, res) => {
-      process.env.TOKEN = res.body.token;
-      done();
-    });
+	chai.request(server)
+		.post("/api/login")
+		.send({
+			email: process.env.EMAIL,
+			password: process.env.PASSWORD,
+		})
+		.end((err, res) => {
+			process.env.TOKEN = res.body.token;
+			done();
+		});
 });
 describe("SignUpUser", () => {
-  it("should sign up a user", (done)=> {
-    chai.request(server)
-      .post("/api/signup")
-      .send({
-        email: "testuser@gmail.com",
-        password: "strongpassword123",
-        name:"test user"
-      })
-      .end((req, res) => {
-        res.should.have.status(200)
-        done()
-      })
-  })
-})
+	it("should sign up a user", (done) => {
+		chai.request(server)
+			.post("/api/signup")
+			.send({
+				email: "testuser@gmail.com",
+				password: "strongpassword123",
+				name: "test user",
+			})
+			.end((req, res) => {
+				res.should.have.status(200);
+				done();
+			});
+	});
+});
 describe("LoginUser", () => {
-  it("should login a user", (done) => {
-    chai.request(server)
-      .post("/api/login")
-      .send({
-        email: "testuser@gmail.com",
-        password:"strongpassword123"
-      })
-      .end((err, res) => {
-        res.should.have.status(200)
-        done();
-      });
-  })
-})
+	it("should login a user", (done) => {
+		chai.request(server)
+			.post("/api/login")
+			.send({
+				email: "testuser@gmail.com",
+				password: "strongpassword123",
+			})
+			.end((err, res) => {
+				process.env.USER_ID = res.body.data.id;
+				res.should.have.status(200);
+				done();
+			});
+	});
+});
+describe("RemoveUser", () => {
+	it("should remove a user", (done) => {
+		chai.request(server)
+			.delete(`/api/users/${process.env.USER_ID}`)
+			.set("x-auth-token", process.env.TOKEN)
+			.end((err, res) => {
+				res.should.have.status(200);
+				done();
+			});
+	});
+});
