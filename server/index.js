996bad235d1099b408b74f444e5380097450e3c3
Ntandoyenkosi M <s.ntando99@gmail.com>
2023-08-29 19:23:16 +0200

    created server folder

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const app = express();
+const router = require("./routes/index");
+const cors = require("cors");
+const path = require("path");
+var PORT = 3001;
+process.env.NODE_ENV === "test" ? (PORT = 3002) : process.env.PORT || 3001;
+require("dotenv").config();
+console.log(process.env.MONGODB_URI);
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+app.use(router);
+// if (process.env.NODE_ENV === "test") {
+// 	PORT = 3002
+// }
+//if (process.env.NODE_ENV === "production") {
+app.use(express.static(path.join(__dirname, "../client/build")));
+//}
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+module.exports = server;
