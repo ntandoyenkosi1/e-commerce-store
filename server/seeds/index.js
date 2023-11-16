1ff734dabd914c009b2b95ab3ac6de78374f6a33
Ntandoyenkosi <nmendu@2u.com>
2023-11-16 23:02:31 +0200

    Added seeds

--- server/seeds/index.js
+++ server/seeds/index.js
@@ -17,72 +17,72 @@ const mongoose = require("mongoose");
 
 // Database connection
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/e-commerce-db",
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/e-commerce-db1",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
 );
 
-// User.insertMany(users)
-//   .then((users) => {
-//     console.log("1. Users Added");
-//     Category.insertMany(categories)
-//       .then((categories) => {
-//         console.log("2. Categories added");
-//         products = products.map((product) => {
-//           product.category =
-//             categories[Math.floor(Math.random() * categories.length)]._id;
-//           return product;
-//         });
-//         //console.log(products);
-//         Product.insertMany(products)
-//           .then((products) => {
-//             console.log("3. Products added");
-//             sales = sales.map((sale) => {
-//               sale.user = users[Math.floor(Math.random() * users.length)]._id;
-//               sale.product =
-//                 products[Math.floor(Math.random() * products.length)]._id;
-//               return sale;
-//             });
-//             Sale.insertMany(sales)
-//               .then((sales) => {
-//                 console.log("4. Sales added");
-//                 shipping.map((ship) => {
-//                   ship.user =
-//                     users[Math.floor(Math.random() * users.length)]._id;
-//                   ship.sale =
-//                     sales[Math.floor(Math.random() * sales.length)]._id;
-//                   return sale;
-//                 });
-//                 Shipping.insertMany(shipping)
-//                   .then((res) => {
-//                     console.log("5. Shipping added");
-//                   })
-//                   .catch((err) => {
-//                     console.log(err);
-//                   });
-//               })
-//               .catch((err) => {
-//                 console.log(err);
-//               });
-//           })
-//           .catch((err) => {
-//             console.log(err);
-//           });
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//       });
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
+User.insertMany(users)
+  .then((users) => {
+    console.log("1. Users Added");
+    Category.insertMany(categories)
+      .then((categories) => {
+        console.log("2. Categories added");
+        products = products.map((product) => {
+          product.category =
+            categories[Math.floor(Math.random() * categories.length)]._id;
+          return product;
+        });
+        //console.log(products);
+        Product.insertMany(products)
+          .then((products) => {
+            console.log("3. Products added");
+            sales = sales.map((sale) => {
+              sale.user = users[Math.floor(Math.random() * users.length)]._id;
+              sale.product =
+                products[Math.floor(Math.random() * products.length)]._id;
+              return sale;
+            });
+            Sale.insertMany(sales)
+              .then((sales) => {
+                console.log("4. Sales added");
+                shipping.map((ship) => {
+                  ship.user =
+                    users[Math.floor(Math.random() * users.length)]._id;
+                  ship.sale =
+                    sales[Math.floor(Math.random() * sales.length)]._id;
+                  return ship;
+                });
+                Shipping.insertMany(shipping)
+                  .then((res) => {
+                    console.log("5. Shipping added");
+                  })
+                  .catch((err) => {
+                    console.log(err);
+                  });
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-// Payment.insertMany(payments)
-//   .then((res) => {
-//     console.log("6. Payments added");
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
+Payment.insertMany(payments)
+  .then((res) => {
+    console.log("6. Payments added");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
